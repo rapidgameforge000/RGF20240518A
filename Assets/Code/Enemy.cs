39b8bc5798368311dcb7a7c1959caa6ce452bab7
tcagame <tcagame@tca.ac.jp>
2024-05-18 14:27:00 +0900

    no message

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -2,30 +2,46 @@ namespace Assets.Code
 {
     internal class Enemy
     {
-
+        enum TYPE
+        {
+            FALL,
+            ADULATION,
+            MAX,
+        };
         const double START_HP = 5;
 
         ItemManager _item_manager;
         private UnityEngine.GameObject _object;
         private double _hp;
         bool _isAlive;
-        int _type;
+        TYPE _type;
 
         internal void initialize(ItemManager item_manager)
         {
             _item_manager= item_manager;
-
+            _type = (TYPE)UnityEngine.Random.Range((int)TYPE.FALL, (int)TYPE.MAX);
             UnityEngine.GameObject prefab = UnityEngine.Resources.Load<UnityEngine.GameObject>("Enemy");
             UnityEngine.GameObject instance = UnityEngine.Object.Instantiate(prefab, SampleScene.Canvas.transform);
             _object = instance;
-            float pos_x = UnityEngine.Random.Range(-960, 960);
-            float pos_y = UnityEngine.Random.Range(-540, 540);
-            UnityEngine.Vector2 pos= new UnityEngine.Vector2(pos_x, pos_y);
-            _object.transform.localPosition = pos;
+            switch (_type) {
+                case TYPE.FALL:
+                    initFallType();
+                    break;
+                case TYPE.ADULATION:
+                    break;
+            }
             _hp = START_HP;
             _isAlive = true;
         }
-        
+
+        private void initFallType()
+        {
+            float pos_x = UnityEngine.Random.Range(-960, 960);
+            float pos_y = UnityEngine.Random.Range(-540, 540);
+            UnityEngine.Vector2 pos = new UnityEngine.Vector2(pos_x, pos_y);
+            _object.transform.localPosition = pos;
+        }
+
         internal void process()
         {
 
@@ -33,7 +49,17 @@ namespace Assets.Code
 
         internal bool isHit(UnityEngine.Vector2 pos)
         {
-            return false;
+            bool hit = false;
+            UnityEngine.Vector2 my_pos = _object.transform.localPosition;
+            UnityEngine.Vector2 distance = pos - my_pos;
+            int hit_range = 40 + 10;
+            float sqr_distance = distance.sqrMagnitude;
+            float sqr_hit_distance = hit_range * hit_range;
+            if(sqr_distance< sqr_hit_distance)
+            {
+                hit = true;
+            }
+            return hit;
         }
         
         internal void hit(double damage)
