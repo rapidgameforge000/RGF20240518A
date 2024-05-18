424c4d434a4e35bf81abcae8c29d372c3d5b5a01
tcagame <tcagame@tca.ac.jp>
2024-05-18 12:58:33 +0900

    BulletManager生成

--- Assets/Code/SampleScene.cs
+++ Assets/Code/SampleScene.cs
@@ -3,12 +3,15 @@ namespace Assets.Code
     internal class SampleScene : UnityEngine.MonoBehaviour
     {
         Enemy _enemy;
+        BulletManager _bullet_mgr;
         private void Awake()
         {
             UnityEngine.QualitySettings.vSyncCount = 0;
             UnityEngine.Application.targetFrameRate = 30;
             _enemy= new Enemy();
             _enemy.initialize();
+            _bullet_mgr = new BulletManager();
+            _bullet_mgr.initialize();
         }
 
         private void Update()
@@ -19,6 +22,7 @@ namespace Assets.Code
         private void doProcess()
         {
             _enemy.process();
+            _bullet_mgr.process();
         }
     }
 }
