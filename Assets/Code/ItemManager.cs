c15f5150390425583a4b1c64a5c39db500d1a9dc
tcagame <tcagame@tca.ac.jp>
2024-05-18 15:35:31 +0900

    当たり判定

--- Assets/Code/ItemManager.cs
+++ Assets/Code/ItemManager.cs
@@ -29,7 +29,7 @@ namespace Assets.Code
             {
                 _items[i].process();
             }
-
+            doHit();
             doCleanUp();
         }
 
@@ -64,17 +64,15 @@ namespace Assets.Code
             _items.RemoveAll(a => a == null);
         }
 
-        //private void doHit( ) {
-        //    for (int i = 0; i < _items.Count; i++) {
-        //        UnityEngine.Vector2 distance = _items[i].getPosition() - _player.GetPlayerPosition();
-        //        int hit_distance = PLAYER_RADIUS + ITEM_RADIUS;
-        //        int sqr_distance = ( int )distance.sqrMagnitude;
-        //        int sqr_hit_distance = hit_distance * hit_distance;
-        //        if (sqr_distance < sqr_hit_distance)
-        //        {
-        //            _items[i].death();
-        //        }
-        //    }       
-        //}
+        private void doHit()
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                if (_player.isHit(_items[i].getPosition(), ITEM_RADIUS)) {
+                    _player.touchedItem(_items[ i ] );
+                    _items[i].death();
+                }
+            }
+        }
     }
 }
